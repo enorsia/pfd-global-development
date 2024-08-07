efa9578626f719067ebc905964ba150387acfcb3
Proshanta15 <roy15-6997@diu.edu.bd>
2024-08-07 18:07:25 +0600

    map edit

--- resources/views/frontend/layout/script.blade.php
+++ resources/views/frontend/layout/script.blade.php
@@ -1,194 +1,207 @@
  <!--- Toggler close icon close button show js code-->
  <script>
-    $(document).ready(function() {
-        $(".navbar-toggler").click(function() {
-            $(this).toggleClass("collapsed");
-        });
-    });
-</script>
+     $(document).ready(function() {
+         $(".navbar-toggler").click(function() {
+             $(this).toggleClass("collapsed");
+         });
+     });
+ </script>
 
-<!-- Header fixed js codeee-->
-<script>
-    window.addEventListener("scroll", function() {
-        var header = document.getElementById("header");
-        if (window.scrollY > 200) {
-            header.classList.add("header-fixed");
-        } else {
-            header.classList.remove("header-fixed");
-        }
-    });
-</script>
+ <!-- Header fixed js codeee-->
+ <script>
+     window.addEventListener("scroll", function() {
+         var header = document.getElementById("header");
+         if (window.scrollY > 200) {
+             header.classList.add("header-fixed");
+         } else {
+             header.classList.remove("header-fixed");
+         }
+     });
+ </script>
  <!-- Form validation js code -->
  <script>
-    document.getElementById("name").addEventListener("input", function () {
-      validateName();
-    });
+     document.getElementById("name").addEventListener("input", function() {
+         validateName();
+     });
 
-    document.getElementById("email").addEventListener("input", function () {
-      validateEmail();
-    });
+     document.getElementById("email").addEventListener("input", function() {
+         validateEmail();
+     });
 
-    document.getElementById("message").addEventListener("input", function () {
-      validateMessage();
-    });
 
-    document
-      .getElementById("contactForm")
-      .addEventListener("submit", function (event) {
-        event.preventDefault();
-        var isValid = validateName() & validateEmail() & validateMessage();
+     document.getElementById("message").addEventListener("input", function() {
+         validateMessage();
+     });
 
-        if (!isValid) {
-            return false;
-        }else{
-            let formData = {
-                _token: $('input[name="_token"]').val(),
-                name: $('#name').val(),
-                email: $('#email').val(),
-                phone: $('#phone').val(),
-                message: $('#message').val()
-            };
+     document
+         .getElementById("contactForm")
+         .addEventListener("submit", function(event) {
+             event.preventDefault();
+             var isValid = validateName() & validateEmail() & validateMessage();
 
-            $.ajax({
-                url: $(this).attr('action'),
-                type: 'POST',
-                data: formData,
-                success: function (response) {
-                    showAlert('success', response.message);
-                    $('#contactForm')[0].reset();
-                },
-                error: function (xhr) {
-                    let errors = xhr.responseJSON.errors;
-                    showAlert('danger', 'There were some errors in your submission.');
-                }
-            });
-        }
-      });
+             if (!isValid) {
+                 return false;
+             } else {
+                 var submitBtn = document.getElementById("submitBtn");
+                 var spinner = submitBtn.querySelector(".spinner-border");
 
-      function showAlert(type, message) {
-            let alertBox = `<div class="alert alert-${type} alert-dismissible fade show" role="alert">
+                 submitBtn.disabled = true;
+                 submitBtn.innerHTML = ` <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+                            Sending`;
+                 let formData = {
+                     _token: $('input[name="_token"]').val(),
+                     name: $('#name').val(),
+                     email: $('#email').val(),
+                     phone: $('#phone').val(),
+                     message: $('#message').val()
+                 };
+
+                 $.ajax({
+                     url: $(this).attr('action'),
+                     type: 'POST',
+                     data: formData,
+                     success: function(response) {
+                         showAlert('success', response.message);
+                         $('#contactForm')[0].reset();
+                     },
+                     error: function(xhr) {
+                         let errors = xhr.responseJSON.errors;
+                         showAlert('danger', 'There were some errors in your submission.');
+                     },
+                     complete: function() {
+                         submitBtn.disabled = false;
+                         submitBtn.innerHTML = "SEND";
+                     }
+                 });
+             }
+         });
+
+     function showAlert(type, message) {
+         let alertBox = `<div class="alert mt-4 alert-${type} alert-dismissible fade show" role="alert">
                                 ${message}
-                                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"><i class="fa-solid fa-xmark"></i></button>
                             </div>`;
-            $('#alert-container').html(alertBox);
-            setTimeout(function () {
-                $('.alert').alert('close');
-            }, 10000);
-        }
+         $('#alert-container').html(alertBox);
+         setTimeout(function() {
+             $('.alert').alert('close');
+         }, 10000);
+     }
 
-    function validateName() {
-      var name = document.getElementById("name").value.trim();
-      var nameError = document.getElementById("name-error");
+     function validateName() {
+         var name = document.getElementById("name").value.trim();
+         var nameError = document.getElementById("name-error");
 
-      if (name.length < 2) {
-        nameError.textContent = "Name must be at least 2 characters long.";
-        return false;
-      } else {
-        nameError.textContent = "";
-        return true;
-      }
-    }
+         if (name.length < 2) {
+             nameError.textContent = "Name must be at least 2 characters long.";
+             return false;
+         } else {
+             nameError.textContent = "";
+             return true;
+         }
+     }
 
-    function validateEmail() {
-      var email = document.getElementById("email").value.trim();
-      var emailError = document.getElementById("email-error");
-      var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
 
-      if (!emailPattern.test(email)) {
-        emailError.textContent = "Please enter a valid email address.";
-        return false;
-      } else {
-        emailError.textContent = "";
-        return true;
-      }
-    }
 
-    function validateMessage() {
-      var message = document.getElementById("message").value.trim();
-      var messageError = document.getElementById("message-error");
+     function validateEmail() {
+         var email = document.getElementById("email").value.trim();
+         var emailError = document.getElementById("email-error");
+         var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
 
-      if (message.length === 0) {
-        messageError.textContent = "Message cannot be empty.";
-        return false;
-      } else {
-        messageError.textContent = "";
-        return true;
-      }
-    }
-  </script>
-<!-- Bottom to top js code-->
-<script>
-    document.addEventListener("DOMContentLoaded", function() {
-        const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+         if (!emailPattern.test(email)) {
+             emailError.textContent = "Please enter a valid email address.";
+             return false;
+         } else {
+             emailError.textContent = "";
+             return true;
+         }
+     }
 
-        window.onscroll = function() {
-            if (
-                document.body.scrollTop > 300 ||
-                document.documentElement.scrollTop > 300
-            ) {
-                scrollToTopBtn.style.display = "block";
-            } else {
-                scrollToTopBtn.style.display = "none";
-            }
-        };
+     function validateMessage() {
+         var message = document.getElementById("message").value.trim();
+         var messageError = document.getElementById("message-error");
 
-        scrollToTopBtn.addEventListener("click", function() {
-            window.scrollTo({
-                top: 0,
-                behavior: "smooth",
-            });
-        });
-    });
-</script>
+         if (message.length === 0) {
+             messageError.textContent = "Message cannot be empty.";
+             return false;
+         } else {
+             messageError.textContent = "";
+             return true;
+         }
+     }
+ </script>
+ <!-- Bottom to top js code-->
+ <script>
+     document.addEventListener("DOMContentLoaded", function() {
+         const scrollToTopBtn = document.getElementById("scrollToTopBtn");
 
-<!-- Click to go the section js code -->
+         window.onscroll = function() {
+             if (
+                 document.body.scrollTop > 300 ||
+                 document.documentElement.scrollTop > 300
+             ) {
+                 scrollToTopBtn.style.display = "block";
+             } else {
+                 scrollToTopBtn.style.display = "none";
+             }
+         };
 
-<script>
-    document.addEventListener("DOMContentLoaded", function() {
-        const header = document.getElementById("header");
-        const navbarSupportedContent = document.getElementById(
-            "navbarSupportedContent"
-        );
-        const navLinks = document.querySelectorAll(
-            ".nav-link[data-target], .dropdown-item[data-target]"
-        );
+         scrollToTopBtn.addEventListener("click", function() {
+             window.scrollTo({
+                 top: 0,
+                 behavior: "smooth",
+             });
+         });
+     });
+ </script>
 
-        navLinks.forEach((link) => {
-            link.addEventListener("click", function(event) {
-                event.preventDefault();
+ <!-- Click to go the section js code -->
 
-                const targetId = link.getAttribute("data-target");
-                const targetSection = document.getElementById(targetId);
+ <script>
+     document.addEventListener("DOMContentLoaded", function() {
+         const header = document.getElementById("header");
+         const navbarSupportedContent = document.getElementById(
+             "navbarSupportedContent"
+         );
+         const navLinks = document.querySelectorAll(
+             ".nav-link[data-target], .dropdown-item[data-target]"
+         );
 
-                if (targetSection) {
-                    navbarSupportedContent.classList.add("hide");
-                    const headerHeight = header.offsetHeight;
-                    const targetPosition = targetSection.offsetTop;
+         navLinks.forEach((link) => {
+             link.addEventListener("click", function(event) {
+                 event.preventDefault();
 
-                    window.scrollTo({
-                        top: targetPosition - headerHeight,
-                        behavior: "smooth",
-                    });
-                }
-            });
-        });
-    });
-</script>
+                 const targetId = link.getAttribute("data-target");
+                 const targetSection = document.getElementById(targetId);
 
-<!--- Tab list section js code -->
-<script>
-    $(".sexytabs").tabs({
-        show: {
-            effect: "slide",
-            direction: "left",
-            duration: 200,
-            easing: "easeOutBack",
-        },
-        hide: {
-            effect: "slide",
-            direction: "right",
-            duration: 200,
-            easing: "easeInQuad",
-        },
-    });
-</script>
+                 if (targetSection) {
+                     navbarSupportedContent.classList.add("hide");
+                     const headerHeight = header.offsetHeight;
+                     const targetPosition = targetSection.offsetTop;
+
+                     window.scrollTo({
+                         top: targetPosition - headerHeight,
+                         behavior: "smooth",
+                     });
+                 }
+             });
+         });
+     });
+ </script>
+
+ <!--- Tab list section js code -->
+ <script>
+     $(".sexytabs").tabs({
+         show: {
+             effect: "slide",
+             direction: "left",
+             duration: 200,
+             easing: "easeOutBack",
+         },
+         hide: {
+             effect: "slide",
+             direction: "right",
+             duration: 200,
+             easing: "easeInQuad",
+         },
+     });
+ </script>
